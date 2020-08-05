78e78bb71f73ce3e7fa7bc54421f3a97ce46abe1
Paulina <paulina12061993@gmail.com>
2020-08-05 21:28:23 +0200

    heroku_v5

--- Books_REST_API/settings.py
+++ Books_REST_API/settings.py
@@ -80,7 +80,6 @@ except ModuleNotFoundError:
     print("Uzupełnij dane i spróbuj ponownie!")
     exit(0)
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
