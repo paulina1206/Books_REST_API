901c73051bf932981128a79939a224a726013a71
Paulina <paulina12061993@gmail.com>
2020-08-05 20:21:39 +0200

    heroku

--- Books_REST_API/settings.py
+++ Books_REST_API/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,3 +119,5 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+# Activate Django-Heroku.
+django_heroku.settings(locals())
